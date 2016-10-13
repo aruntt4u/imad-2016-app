f234c725f8e5b5fd5c5e6e9c5202247eb8992c43
aruntt4u <aruntt4u@gmail.com>
2016-10-13 19:58:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,15 +4,108 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone = {
+    title: 'Arun Resume',
+    Heading: `<div>
+            <p>
+            <br>
+            <br>
+            Name: Arun<br>
+            aruntt4u@gmail.com<br>
+            9164282828<br>
+            <br>
+            <br>
+            Gadag,Karnataka
+            </p>
+            <hr>
+        </div>`,
+    summary: `<div>
+            <h2 align=left>
+            Summary    
+            </h2>
+            <P>
+                This is my summary. This is my summary. This is my summary. This is my summary. This is my summary. This is my summary. This is my summary. This is my summary. This is my summary. This is my summary. This is my summary. This is my summar.
+            </P>
+            <hr>
+        </div>`,
+    work: `<div>
+            <h2 aligh=left>
+                Work Experience
+            </h2>
+            <h3>
+                Student<br>
+                place: college and home
+            </h3>
+            <p>I work in my home and college. very difficult. Ha ha ha......</p>
+            <dir>
+                <li>My school</li>
+                <li>My pu college</li>
+                <li>My engineering college </li>    
+            </dir>
+            <hr>
+            </div>`,
+    education:`<div>
+                <h2>
+                    Education
+                </h2>
+                <p>
+                    That's what i am doing.<br>
+                    learnt lot of things in my school<br>
+                    Studied a bit of science in pu college<br>
+                    not attended regular classes in engineering to learn modern application development<br> 
+                </p>
+                <hr>
+            </div>`,
+    aditional:`<div>
+                <h2>
+                    Additional Information
+                </h2>
+                <p>
+                    I dont't have much addition informational about me right now.<br>
+                    When i get those additional informations, i will definitely upload.
+                    That's all for now. thank u.
+                </p>
+            </div>`
+};
 
-var htmltemplet = ''
+function createtemplate (data){
+ var title = data.title;
+ var heading = data.heading;
+ var summary = data.summary;
+ var work = data.work;
+ var additional= data.additional;
+var htmltemplate = ` <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+<body>
+        <h1 align=center>
+            RESUME
+        </h1>
+        <font size=4>
+        <div class="center">
+            <img src="/ui/arun.png" class="img-small" align=right>
+        </div>
+        ${heading}
+        ${summary}
+        ${work}
+        ${additional}
+        <script type="text/javascript" src="/ui/arun.png">
+        </script>
+    </body>
+</html>
+`;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createtemplate(articleone));
 });
 
 app.get('/ui/style.css', function (req, res) {
