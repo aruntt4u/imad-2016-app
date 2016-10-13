2a9c6f34dff025962052a0858cb0888db29cf613
aruntt4u <aruntt4u@gmail.com>
2016-10-13 20:11:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,68 @@ var articleone = {
             </div>`
 };
 
+var articletwo = {
+    title: 'madi Resume',
+    Heading: `<div>
+            <p>
+            <br>
+            <br>
+            Name: madi<br>
+            madi@gmail.com<br>
+            999999999<br>
+            <br>
+            <br>
+             india
+            </p>
+            <hr>
+        </div>`,
+    summary: `<div>
+            <h2 align=left>
+            Summary    
+            </h2>
+            <P>
+                This is madi summary. This is madi summary. This is madi summary. This is madi summary. This is madi summary. This is madi summary. This is madi summary. This is madi summary. This is madi summary. This is madi summary. This is madi summary. This is madi summary.
+            </P>
+            <hr>
+        </div>`,
+    work: `<div>
+            <h2 aligh=left>
+                Work Experience
+            </h2>
+            <h3>
+                Student<br>
+                place: college and home
+            </h3>
+            <p>madi work at IMAD. Ha ha ha......</p>
+            <dir>
+                <li>Madi school</li>
+                <li>Madi college</li>
+                <li>Madi iit college </li>    
+            </dir>
+            <hr>
+            </div>`,
+    education:`<div>
+                <h2>
+                    Education
+                </h2>
+                <p>
+                    I have lot of education<br>
+                    learnt lot of things in my school<br>
+                    Studied a bit of science in pu college<br>
+                    not attended regular classes in engineering to learn modern application development<br> 
+                </p>
+                <hr>
+            </div>`,
+    aditional:`<div>
+                <h2>
+                    Additional Information
+                </h2>
+                <p>
+                    i conduct imad course for students. I am very popular and star figure in the internet.
+                </p>
+            </div>`
+};
+
 function createtemplate (data) {
  var title = data.title;
  var heading = data.heading;
@@ -109,6 +171,10 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function (req, res) {
   res.send(createtemplate(articleone));
+});
+
+app.get('/article-two', function (req, res) {
+  res.send(createtemplate(articletwo));
 });
 
 app.get('/ui/style.css', function (req, res) {
